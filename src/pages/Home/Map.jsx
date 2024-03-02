d72495a8f19bd8145eaaecbcb1b9d5d7ac6576df
Raouf <raoufslv09@gmail.com>
2024-03-02 06:13:05 +0100

    getting react map

--- src/pages/Home/Map.jsx
+++ src/pages/Home/Map.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const Map = () => {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
   return (
     <>
-    <div className='yesflex yesjustify-center yesp-20'>
-
-    <h1 className='yestext-5xl yesfont-bold'> Map</h1>
-    </div>
-    <div className='yesflex'>
-      <img className=' ' src='/Vector 2.svg'/>
-      <div className='yesflex yesjustify-center'>
-
-        <img className='yesabsolute ' src="/Map.svg" />
+      <h1 className="yestext-5xl yesfont-bold yesflex yesjustify-center yesm-20">
+        {" "}
+        Map
+      </h1>
+      <div className="yesh-screen yesw-screen yesp-16">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent
+            lat={119.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
